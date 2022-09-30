6a27ba4cdbd40116c4167e4775bd7fdd8cf6b724
21CE020 <113235967+21CE020@users.noreply.github.com>
2022-09-30 23:22:09 +0530

    Add files via upload

--- Runable_HelloWorld.java
+++ Runable_HelloWorld.java
@@ -1,15 +1,13 @@
-class MyThreadRunable1 implements Runnable
-{
-    public void run()
-    {
+class MyThreadRunable1 implements Runnable {
+    public void run() {
         for (int i = 0; i < 2; i++) {
             System.out.println("Hello World");
         }
         System.out.println("End of MyThreadRunable1");
     }
 }
- 
-public class Runalbe_HelloWorld {
+
+public class Runable_HelloWorld {
     public static void main(String[] args) {
         MyThreadRunable1 M1 = new MyThreadRunable1();
         Thread T1 = new Thread(M1);
